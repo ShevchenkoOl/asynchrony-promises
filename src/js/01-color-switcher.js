b2610dd5d9f62f5062eddfbc47e9c72ed699c2d6
Oleksii Shevchenko <uzlabini@gmail.com>
2022-10-05 22:49:09 +0200

    05.10.2022

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', changeColor)
+
+function changeColor(){
+   changeColor = setInterval(() => {body.style.background =`#${Math.floor(Math.random() * 16777215).toString(16)}`}, 1000)
+   btnStart.disabled = true;
+   btnStop.disabled = false;
+}
+
+btnStop.addEventListener("click", () => {
+    clearInterval(changeColor);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+  });
